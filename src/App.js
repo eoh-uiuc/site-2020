03efbecddc1655b9ee85a9e4ab74ddec652ceecd
Lauren Ho <laurenh4@illinois.edu>
2019-09-09 10:26:07 -0500

    Splash and travis

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo-white.png';
 import './App.css';
 
 function App() {
@@ -7,17 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Engineering Open House</h1>
+        <h2>March 27 - 28, 2020</h2>
       </header>
     </div>
   );
