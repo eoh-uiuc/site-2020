1f7340c8c4e04979b315a0afed2bf32322a4f13d
Lauren Ho <laurenh4@illinois.edu>
2019-09-21 14:11:43 -0500

    Update index, splash

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Engineering Open House</h1>
-        <h2>March 27 - 28, 2020</h2>
+        <h2>University of Illinois at Urbana-Champaign</h2>
+        <h3>March 27 - 28, 2020</h3>
       </header>
     </div>
   );
