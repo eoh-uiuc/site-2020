96685a0410ceb22aebcf2da1c82bd8b4aa7a2c49
Lauren Ho <laurenh4@illinois.edu>
2020-01-27 19:42:42 -0600

    Speaker series event

--- src/scenes/Events/Events.jsx
+++ src/scenes/Events/Events.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import InfoPane from 'components/InfoPane';
 
+import defaultImage from 'assets/images/eoh-sign.jpg';
 import startupImage from 'assets/images/startup_showcase.jpg';
 
 const EventSection = () => (
@@ -18,6 +19,17 @@ const EventSection = () => (
         The Startup Showcase highlights Illinois innovation through booths hosted by startups founded
         by current students, alumni, and faculty. Stop by to discover new technologies and ideas, or
         sign up <a href="https://forms.gle/wcXKnBZJKvrXh76D7" target="_blank" rel="noopener noreferrer">here</a> to show off yours!
+      </p>
+    </InfoPane>
+    <InfoPane
+      image={defaultImage}
+      name="Speaker Series"
+    >
+      <p>
+      We have a series of exciting panels lined up for this year’s EOH! Our topics include a 
+      Century of Innovation, what do you want to be when you grow up?, Student Leaders, and Startups. 
+      Join us to hear about the experiences of students, alums, and professors in these areas. 
+      If you are interested in being on one of our panels, please fill out <a href="https://docs.google.com/forms/u/2/d/e/1FAIpQLScnempdbGkpxP_56lnvc_xT2NW9BvFncssmR8L6sTj8LhjxWg/viewform?usp=send_form" target="_blank" rel="noopener noreferrer">this form</a>.
       </p>
     </InfoPane>
   </div>
